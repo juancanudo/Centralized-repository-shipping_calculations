83f4f45bd4899ba447992e82e2ba68670c9b7219
Juancanudo <juan.carlos.nuno@outlook.com>
2025-06-11 18:52:28 -0400

    agregada liena adicional

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,5 +1,6 @@
 # Shipping Cost Calculator
 # Aquí hay una nueva actualización por juancanudo
+# Here is another update by juancanudo
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
